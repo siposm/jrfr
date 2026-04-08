d685e9399b24ffd344bacea271211333fa5f14e0
Sipos Miklos <commit@siposm.hu>
2026-04-08 19:38:50 +0200

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "homepage", pathMatch: "full" },
+  { path: "list", component: ListComponent },
+  { path: "homepage", component: HomepageComponent },
+  { path: "**", redirectTo: "homepage", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
