94e2d041669b3b79b0e06bdb0c5db2a31b1d95fb
Sipos Miklos <commit@siposm.hu>
2026-04-13 17:25:13 +0200

    HUF tolocalestring added

--- src/app/developer.ts
+++ src/app/developer.ts
@@ -4,21 +4,10 @@ export class Developer {
     salary: number = 0
 
     getFormattedSalary(): string {
-        let out = ""
-        for (let i = 0; i < this.salary.toString().length; i++) {
-            out += this.salary.toString()[i]
-            if (i == 2) {
-                out += " "
-            }
-        }
-        return out + " HUF"
-
-        // házi feladat!!!
-        // 4000000 -> 4.000.000
-        // 400000 -> 400.000
-        // 40000 -> 40.000
-        // 4000 -> 4000
-        // 400 -> 400
-        // 40 -> 40
+        return this.salary.toLocaleString("hu-HU", {
+            style: "currency",
+            currency: "HUF",
+            maximumFractionDigits: 0
+        })
     }
 }
