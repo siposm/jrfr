882a15cf991073c1b7236121f8951da7d3e00298
Sipos Miklos <commit@siposm.hu>
2026-04-15 19:09:32 +0200

    developer class extended

--- src/app/developer.ts
+++ src/app/developer.ts
@@ -6,6 +6,27 @@ export class Developer {
     job: string = ""
     age: number | null = null
     salary: number | null = null
+    skills: string[] = []
+
+    public get skillsAsString(): string {
+        // olvasás
+        return this.skills.join(",")
+    }
+
+    public set skillsAsString(value: string) {
+        // írás
+        // bemenet: alma,körte,valami --> ["alma","körte","valami"]
+        this.skills = value.split(",")
+    }
+
+    getSkillsFormatted(): string {
+        return this.skills.join(" • ")
+        // let result = ""
+        // for (let i = 0; i < this.skills.length; i++) {
+        //     result += this.skills[i] + ", "
+        // }
+        // return result.slice(0, -2)
+    }
 
     getShortId(): string {
         // 5e3d8ab3-7c88-41ed-11f1-21c1e6984604
